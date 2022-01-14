16970915586e55e2e464c6c09192b58b473dcb7a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-14 18:27:46 +0900

    [#5 fix] 일련번호 리팩터링

--- src/main/java/org/jfree/date/SpreadsheetDate.java
+++ src/main/java/org/jfree/date/SpreadsheetDate.java
@@ -146,7 +146,7 @@ public class SpreadsheetDate extends DayDate {
      */
     public SpreadsheetDate(final int serial) {
 
-        if ((serial >= SERIAL_LOWER_BOUND) && (serial <= SERIAL_UPPER_BOUND)) {
+        if ((serial >= EARLIEST_DATE_ORDINAL) && (serial <= LATEST_DATE_ORDINAL)) {
             this.serial = serial;
         }
         else {
@@ -156,7 +156,7 @@ public class SpreadsheetDate extends DayDate {
 
         // the day-month-year needs to be synchronised with the serial number...
       // get the year from the serial date
-      final int days = this.serial - SERIAL_LOWER_BOUND;
+      final int days = this.serial - EARLIEST_DATE_ORDINAL;
       // overestimated because we ignored leap days
       final int overestimatedYYYY = 1900 + (days / 365);
       final int leaps = DayDate.leapYearCount(overestimatedYYYY);
