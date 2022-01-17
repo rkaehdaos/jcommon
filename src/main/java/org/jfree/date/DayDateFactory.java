77f97562d83fc07a906cedfecf7c73934b34a1ea
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-17 17:23:48 +0900

    [#5 fix] DayDateFactory 생성
    - ABSTRACT FACTORY PATTERN 적용
    - DayDate 인스턴스를 생성하며 최대날짜, 최소날짜등 구현 관련 질문에 응답
    = createInstance() → makeDate() : 좀 더 서술적인 이름
    - 기본적으로 SpreadSheetDateFactory사용 → 언제든 바꿔도 괜찮음

---
+++ src/main/java/org/jfree/date/DayDateFactory.java
@@ -0,0 +1,42 @@
+package org.jfree.date;
+
+public abstract class DayDateFactory {
+
+    private static DayDateFactory factory = new SpreadSheetDateFactory();
+    public static void setInstance(DayDateFactory factory) {
+        DayDateFactory.factory = factory;
+    }
+
+    protected abstract DayDate _makeDate(int ordinal);
+    protected abstract DayDate _makeDate(int day, Month month, int year);
+    protected abstract DayDate _makeDate(int day, int month, int year);
+    protected abstract DayDate _makeDate(java.util.Date date);
+
+    protected abstract int _getMiniMumYear();
+    protected abstract int _getMaximumYear();
+
+    public static DayDate makeDate(int ordinal) {
+        return factory._makeDate(ordinal);
+    }
+
+    public static DayDate makeDate(int day, Month month, int year) {
+        return factory._makeDate(day, month, year);
+    }
+
+    public static DayDate makeDate(int day, int month, int year) {
+        return factory._makeDate(day, month, year);
+    }
+
+    public static DayDate makeDate(java.util.Date date) {
+        return factory._makeDate(date);
+    }
+
+    public static int getMiniMumYear() {
+        return factory._getMiniMumYear();
+    }
+
+    public static int getMaximumYear() {
+        return factory._getMaximumYear();
+    }
+}
+
