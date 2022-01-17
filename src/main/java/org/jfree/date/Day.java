323585d8661bf3b30ddb70d3d3b24bdf7dc2cbe3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-17 18:07:27 +0900

    [#5 fix] Day 수정

--- src/main/java/org/jfree/date/Day.java
+++ src/main/java/org/jfree/date/Day.java
@@ -2,30 +2,34 @@ package org.jfree.date;
 
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 public enum Day {
-    SUNDAY(1),
-    MONDAY(2),
-    TUESDAY(3),
-    WEDNESDAY(4),
-    THURSDAY(5),
-    FRIDAY(6),
-    SATURDAY(7);
-    public static final DateFormatSymbols
-            DATE_FORMAT_SYMBOLS = new SimpleDateFormat("", Locale.US).getDateFormatSymbols();
+    SUNDAY(Calendar.SUNDAY),
+    MONDAY(Calendar.MONDAY),
+    TUESDAY(Calendar.TUESDAY),
+    WEDNESDAY(Calendar.WEDNESDAY),
+    THURSDAY(Calendar.THURSDAY),
+    FRIDAY(Calendar.FRIDAY),
+    SATURDAY(Calendar.SATURDAY);
+    public static DateFormatSymbols
+            dateFormatSymbols = new SimpleDateFormat("", Locale.US).getDateFormatSymbols();
 
     public final int index;
 
     Day(int index) { this.index = index; }
 
-    public static Day fromInt(int dayIndex){
+    public static Day fromInt(int dayIndex) {
         for (Day day : Day.values()) {
-            if(day.index==dayIndex)
+            if (day.index == dayIndex)
                 return day;
         }
-        throw new IllegalArgumentException("invalid day index " + dayIndex);
+        throw new IllegalArgumentException(
+                String.format("Illegal day index: %d", dayIndex)
+        );
     }
+
     public int toInt() { return index; }
 
     public static Day parse(String s) {
@@ -33,23 +37,24 @@ public enum Day {
         for (Day day : Day.values()) {
             if (day.matches(s)) return day;
         }
-        try {
-            return fromInt(Integer.parseInt(s));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("invalid day" + s);
-        }
+
+        throw new IllegalArgumentException(
+                String.format("%s is not a valid weekday string", s)
+        );
+
     }
 
     private boolean matches(String s) {
         return s.equalsIgnoreCase(toString()) ||
                 s.equalsIgnoreCase(toShortString());
     }
+
     @Override
     public String toString() {
-        return DATE_FORMAT_SYMBOLS.getWeekdays()[index - 1];
+        return dateFormatSymbols.getWeekdays()[index];
     }
 
     private String toShortString() {
-        return DATE_FORMAT_SYMBOLS.getShortWeekdays()[index - 1];
+        return dateFormatSymbols.getShortWeekdays()[index];
     }
 }
