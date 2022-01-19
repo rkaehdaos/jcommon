0f00e677ddf544b956722216d35ad960076fc1ec
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-19 16:27:09 +0900

    [#5 fix] Day 정리

--- src/main/java/org/jfree/date/Day.java
+++ src/main/java/org/jfree/date/Day.java
@@ -13,20 +13,22 @@ public enum Day {
     THURSDAY(Calendar.THURSDAY),
     FRIDAY(Calendar.FRIDAY),
     SATURDAY(Calendar.SATURDAY);
-    public static DateFormatSymbols
-            dateFormatSymbols = new SimpleDateFormat("", Locale.US).getDateFormatSymbols();
 
     private final int index;
+    private static DateFormatSymbols dateFormatSymbols =
+            new SimpleDateFormat("", Locale.US).getDateFormatSymbols();
 
-    Day(int index) { this.index = index; }
+    Day(int day) {
+        this.index = day;
+    }
 
-    public static Day fromInt(int dayIndex) {
-        for (Day day : Day.values()) {
-            if (day.index == dayIndex)
-                return day;
+    public static Day fromInt(int index) throws IllegalArgumentException{
+        for (Day d : Day.values()) {
+            if (d.index == index)
+                return d;
         }
         throw new IllegalArgumentException(
-                String.format("Illegal day index: %d", dayIndex)
+                String.format("Illegal day index: %d", index)
         );
     }
 
